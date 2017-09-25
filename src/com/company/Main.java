5dd43e14998059e68bcc257db4b0bf3491786b26
user <mmelody0328@gmail.com>
2017-09-25 14:44:23 +0800

    modified

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("105024123高敏庭");
+
+    System.out.println("資傳系");
+
+    System.out.println("IC");
+    }
+}
